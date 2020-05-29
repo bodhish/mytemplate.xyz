0fdefa1106f612246775e55b92c228f43bb7a524
Bodhish Thomas <bodhish@gmail.com>
2020-05-30 02:21:27 +0530

    Update tailwind version

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,28 +4,6 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: {
-          100: "#F0EAFB",
-          200: "#D3BFF3",
-          300: "#B08EE6",
-          400: "#976AE2",
-          500: "#6025C0",
-          600: "#4D1E9A",
-          700: "#380D80",
-          800: "#35156B",
-          900: "#1F0D40"
-        },
-        secondary: {
-          100: "#FEE6EF",
-          200: "#FCB5D0",
-          300: "#FF80B0",
-          400: "#F95392",
-          500: "#F61067",
-          600: "#AC0645",
-          700: "#A61149",
-          800: "#7B0531",
-          900: "#4A031E"
-        },
         gray: {
           100: "#FBFAFC",
           200: "#F7F5FA",
@@ -35,9 +13,17 @@ module.exports = {
           600: "#9187A1",
           700: "#7D728F",
           800: "#6A5F7A",
-          900: "#453C52"
-        }
-      }
-    }
-  }
+          900: "#453C52",
+        },
+      },
+    },
+  },
+  purge: {
+    content: ["./src/**/*.html", "./src/**/*.re", "./src/**/*.bs.js"],
+
+    // These options are passed through directly to PurgeCSS
+    options: {
+      whitelistPatterns: [/^bg-/, /^text-/, /^border-/],
+    },
+  },
 };
