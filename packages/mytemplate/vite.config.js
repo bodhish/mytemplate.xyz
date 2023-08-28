34017635c316b9c028d5c265cdaf75827e446f93
Bodhish Thomas <bodhish@gmail.com>
2023-08-28 20:27:50 +0530

    More updates

--- packages/mytemplate/vite.config.js
+++ packages/mytemplate/vite.config.js
@@ -5,4 +5,15 @@ import createReScriptPlugin from "@jihchi/vite-plugin-rescript";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), createReScriptPlugin()],
+  build: {
+    outDir: "build", // Set the build output directory to 'build'
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`,
+      },
+    },
+  },
 });
